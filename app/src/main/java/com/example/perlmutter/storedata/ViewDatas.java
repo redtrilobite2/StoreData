8983c82a5ce4e43bb7090be518b07c3582311f42
Ellie DeSota <desotaelianna@gmail.com>
2017-03-29 11:32:27 -0400

    here

--- app/src/main/java/com/example/perlmutter/storedata/ViewDatas.java
+++ app/src/main/java/com/example/perlmutter/storedata/ViewDatas.java
@@ -1,7 +1,9 @@
 package com.example.perlmutter.storedata;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ViewDatas extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class ViewDatas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_datas);
+    }
+    public void nextPage(View view){
+        Intent intent = new Intent(this, )
+
     }
 }
