a5e6a9f8e05546ae577d1f4a979801295b56363e
Perlmutter <redtrilobite@gmail.com>
2017-04-05 13:16:52 -0400

    Kent 3

---
+++ app/src/main/java/com/example/perlmutter/storedata/entername.java
@@ -0,0 +1,13 @@
+package com.example.perlmutter.storedata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class entername extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.entername);
+    }
+}
