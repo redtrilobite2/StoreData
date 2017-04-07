cb4f7210fd88bd08381899677a9294eef23d6194
Perlmutter <redtrilobite@gmail.com>
2017-04-07 13:12:39 -0400

    Kent 5

--- app/src/main/java/com/example/perlmutter/storedata/ViewData.java
+++ app/src/main/java/com/example/perlmutter/storedata/ViewData.java
@@ -1,15 +1,40 @@
 package com.example.perlmutter.storedata;
 
 import android.content.Intent;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.DataPointInterface;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.series.OnDataPointTapListener;
+import com.jjoe64.graphview.series.Series;
 
 public class ViewData extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_viewdata);
+        GraphView line_graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> line_series =
+                new LineGraphSeries<DataPoint >(new DataPoint[] {
+                        new DataPoint(0, 1),
+                        new DataPoint(1, 5),
+                        new DataPoint(2, 3),
+                        new DataPoint(3, 2),
+                        new DataPoint(4, 6)
+                });
+        line_graph.addSeries(line_series);
+        line_series.setOnDataPointTapListener(new OnDataPointTapListener() {
+            @Override
+            public void onTap(Series series, DataPointInterface dataPoint) {
+                Toast.makeText(ViewData.this, "Series: On Data Point clicked:"  + dataPoint, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     public void toStoreData(View view) {
