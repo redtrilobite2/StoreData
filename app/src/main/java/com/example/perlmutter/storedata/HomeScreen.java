12021e6dd6f15b7d66fd069c1548afab4b0e69b7
Sally <szhi@wpi.edu>
2017-04-25 07:58:44 -0400

    sally9

--- app/src/main/java/com/example/perlmutter/storedata/HomeScreen.java
+++ app/src/main/java/com/example/perlmutter/storedata/HomeScreen.java
@@ -1,15 +1,17 @@
 package com.example.perlmutter.storedata;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
-public class HomeScreen extends AppCompatActivity {
+public class HomeScreen extends Activity implements AdapterView.OnItemSelectedListener {
     public Button makeNewSport;
 
     Spinner spin;
@@ -20,9 +22,22 @@ public class HomeScreen extends AppCompatActivity {
         setContentView(R.layout.activity_homescreen);
 
         spin = (Spinner) findViewById(R.id.spinner);
-        String sports[] = {"Track", "Soccer", "Basketball", "Swimming"};
-        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_dropdown_item_1line, sports);
+        String sports[] = {"Select a sport","Track", "Soccer", "Basketball", "Swimming"};
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, sports);
         spin.setAdapter(adapter);
+        spin.setOnItemSelectedListener(this);
+    }
+    public void onItemSelected(AdapterView<?> parent, View view,
+                               int pos, long id) {
+        pos=spin.getSelectedItemPosition();
+        if (pos!=0) {
+            Intent intent = new Intent(HomeScreen.this, SportHome.class);
+            startActivity(intent);
+        }
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
     }
     public void toSportStyle(View view) {
         Intent intent = new Intent(HomeScreen.this, SportStyle.class);
