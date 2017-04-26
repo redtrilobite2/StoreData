de402920508bfbaf7826c0365bca9d10fc072fa0
Sally <szhi@wpi.edu>
2017-04-26 11:41:01 -0400

    sally10

--- app/src/main/java/com/example/perlmutter/storedata/SportHome.java
+++ app/src/main/java/com/example/perlmutter/storedata/SportHome.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 /**
  * Created by Ellie DeSota on 4/6/2017.
@@ -31,4 +34,11 @@ public class SportHome extends AppCompatActivity {
         Intent intent = new Intent(SportHome.this, HomeScreen.class);
         startActivity(intent);
     }
+
+    /*public void setTextView (View view){
+        TextView sportName=(TextView) findViewById(R.id.SportName);
+        Spinner spin = (Spinner) findViewById(R.id.spinner);
+        String text=spin.getSelectedItem().toString();
+        sportName.setText(text);
+    }*/
 }
