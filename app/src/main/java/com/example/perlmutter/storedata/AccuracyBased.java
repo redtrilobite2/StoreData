22cc14bac3cb4429a5bccf8a3d028dcf56393d84
Sally <szhi@wpi.edu>
2017-05-13 19:10:23 -0400

    sally17

---
+++ app/src/main/java/com/example/perlmutter/storedata/AccuracyBased.java
@@ -0,0 +1,13 @@
+package com.example.perlmutter.storedata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AccuracyBased extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accuracy_based);
+    }
+}
---
+++ app/src/main/java/com/example/perlmutter/storedata/PointBased.java
@@ -0,0 +1,13 @@
+package com.example.perlmutter.storedata;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PointBased extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_point_based);
+    }
+}
